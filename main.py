c8ddac1cf9f6b5d43fb677a081403aa9de086a25
bbtqqrl <makspeys@gmail.com>
2025-04-17 12:04:02 +0200

    Create freame for SQLaclhemy

--- main.py
+++ main.py
@@ -2,11 +2,18 @@ from typing import Annotated
 from fastapi import FastAPI, Path
 from contextlib import asynccontextmanager
 import uvicorn
+from core.models import Base, db_helper
 from interview_sessions import router as router_v1
 from core.config import settings
 
+@asynccontextmanager
+async def lifespan(app: FastAPI):
+    async with db_helper.engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+    yield
 
-app = FastAPI()
+
+app = FastAPI(lifespan=lifespan)
 
 app.include_router(router=router_v1, prefix=settings.api_v1_prefix)
 
