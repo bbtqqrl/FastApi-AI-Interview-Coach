92d3b3a83f1bfd909c2ba2fd0663c7d65de43077
bbtqqrl <makspeys@gmail.com>
2025-04-17 12:56:36 +0200

    alembic initialization

--- core/models/__init__.py
+++ core/models/__init__.py
@@ -2,7 +2,13 @@ __all__ = (
     "Base",
     "DatabaseHelper",
     "db_helper"
+    "User",
+    "Session",
+    "Question",
 )
 
 from .base import Base
 from .db_helper import DatabaseHelper, db_helper
+from .user import User
+from .session import Session
+from .question import Question
